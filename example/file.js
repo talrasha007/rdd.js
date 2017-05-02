5aaf0dda533f207a309a52150a5431770fe4a795
ttl <talrasha007@gmail.com>
2017-05-02 17:03:49 +0800

    writer & promise.

--- example/file.js
+++ example/file.js
@@ -4,6 +4,13 @@ const rdd = RDD.fromTextFile(`${__dirname}/iterable.js`);
 
 rdd.filter(l => l).forEach(console.log);
 
-rdd.filter(l => l)
-  .map(l => ({ lineWordCount: l.split(/\s+/).length }))
-  .forEach(console.log);
+const cntRdd = rdd.filter(l => l)
+  .map(l => ({ lineWordCount: l.split(/\s+/).length }));
+
+cntRdd.forEach(console.log);
+
+rdd.saveAsTextFile('/tmp/foo.txt');
+cntRdd.saveAsJsonFile('/tmp/foo.json').then(() => {
+  console.log('===================');
+  RDD.fromJsonFile('/tmp/foo.json').forEach(console.log);
+});
