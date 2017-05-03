5073b16f3fc193ecf6d3ccfdb8d26ec88b5c6654
ttl <talrasha007@gmail.com>
2017-05-03 15:37:46 +0800

    count groupBy groupByKey reduceByKey

--- example/iterable.js
+++ example/iterable.js
@@ -1,10 +1,28 @@
 const RDD = require('..');
 
-const rdd = RDD.fromIterable([1, 2, 3, 4, 5]);
+(async function () {
+  const rdd = RDD.fromIterable([1, 2, 3, 4, 5]);
 
-rdd.reduce((m, i) => m + i).then(console.log);
+  await rdd.reduce((m, i) => m + i).then(console.log);
 
-rdd
-  .map(i => [i % 2, i * 2])
-  .reduceBy(pair => pair[0], (m, pair) => m + pair[1], 0)
-  .forEach(console.log);
+  await rdd
+    .map(i => [i % 2, i * 2])
+    .reduceBy(pair => pair[0], (m, pair) => m + pair[1], 0)
+    .forEach(console.log);
+
+  await rdd
+    .map(i => [i % 2, i * 2])
+    .reduceByKey((m, v) => m + v)
+    .forEach(console.log);
+
+  await rdd
+    .groupBy(i => i % 2)
+    .forEach(console.log);
+
+  await rdd
+    .map(i => [i % 2, i * 2])
+    .groupByKey()
+    .forEach(console.log);
+
+  console.log(await rdd.count());
+})().catch(e => console.log(e.stack));
